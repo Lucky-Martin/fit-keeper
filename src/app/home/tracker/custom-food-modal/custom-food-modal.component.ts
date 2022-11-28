0a6d79c3fba770d56f03cfc5e6eb3c849a944f4c
Lucky-Martin <l.matev@casualino.com>
2022-11-28 18:21:45 +0200

    Implement food modal v1.2

--- src/app/home/tracker/custom-food-modal/custom-food-modal.component.ts
+++ src/app/home/tracker/custom-food-modal/custom-food-modal.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, ViewChild} from '@angular/core';
 import { IonModal, ModalController } from '@ionic/angular';
 import { UpdateGraphService } from 'src/app/food-search/food-list/food-graph-preview/update-graph.service';
 import { IFetchFoodData } from 'src/app/food-search/food-response.model';
-import { Food } from '../food.model';
+import { Food, Macros } from '../food.model';
 
 @Component({
   selector: 'app-custom-food-modal',
@@ -13,7 +13,8 @@ export class CustomFoodModalComponent {
   @Input() button: boolean = true;
   @ViewChild('modal') modal: IonModal;
   food: Food;
-  foodCopy: Food;
+  quantity: number = 1;
+  private initialMacros: Macros;
 
   constructor(private modalController: ModalController) {
     if (this.food == undefined) {
@@ -22,20 +23,35 @@ export class CustomFoodModalComponent {
   }
 
   onWeightChanged(event) {
-    if (!this.foodCopy) {
-      this.foodCopy = this.food;
+    if (event.target.value < 1) {
+      return this.food.weight = 1;
+    } else if (event.target.value > 1000) {
+      return this.food.weight = 1000;
+    }
+
+    if (!this.initialMacros) {
+      this.initialMacros = this.food.macros;
     }
 
     const multiplier = event.target.value / 100;
 
-    console.log(this.foodCopy);
-    this.food.macros.protein = Math.round(this.foodCopy.macros.protein * multiplier);
-    this.food.macros.carbs = Math.round(this.foodCopy.macros.carbs * multiplier);
-    this.food.macros.fats = Math.round(this.foodCopy.macros.fats * multiplier);
+    const foodCopyJson = JSON.stringify(this.initialMacros);
+
+    this.food.macros.protein = Math.round(this.initialMacros.protein * multiplier);
+    this.food.macros.carbs = Math.round(this.initialMacros.carbs * multiplier);
+    this.food.macros.fats = Math.round(this.initialMacros.fats * multiplier);
+    
+    this.initialMacros = JSON.parse(foodCopyJson);
   }
 
   onMacrosChanged(nutrientName: string, value: number) {
     this.food.macros[nutrientName] = value;
+  }
+
+  updateQuantity(increase: boolean) {
+    if (!increase && this.quantity === 1) return; 
+
+    this.quantity += increase ? 1 : -1;
   }
 
   cancel() {
