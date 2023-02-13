311a8064030fd0fa965bc28ec58d48f72f4a75c4
Lucky-Martin <l.matev@casualino.com>
2023-02-13 08:51:34 +0200

    Build web app

--- src/app/food-search/scan-food-barcode/scan-food-barcode.component.ts
+++ src/app/food-search/scan-food-barcode/scan-food-barcode.component.ts
@@ -4,7 +4,7 @@ import {BarcodeService, IBarcodeProductData} from './barcode.service';
 import {IFetchFoodData} from '../food-response.model';
 import {Food} from '../../home/tracker/food.model';
 import {AddFoodModalComponent} from '../add-food-modal/add-food-modal.component';
-import {ModalController} from '@ionic/angular';
+import {ModalController, ToastController} from '@ionic/angular';
 import {TrackingService} from '../../home/tracker/tracking.service';
 
 @Component({
@@ -17,13 +17,18 @@ export class ScanFoodBarcodeComponent implements OnInit {
   constructor(private barcodeScanner: BarcodeScanner,
               private barcodeService: BarcodeService,
               private modalController: ModalController,
-              private trackingService: TrackingService) { }
+              private trackingService: TrackingService,
+              private toastController: ToastController) { }
 
   ngOnInit() { }
 
   async scanBarcode() {
     this.barcodeScanner.scan().then(barcodeData => {
       this.barcodeService.fetchProductData(barcodeData.text).subscribe(async productData => {
+        if (!productData.product) {
+          await this.displayNotFound();
+          return;
+        }
         await this.addFood(productData);
       });
     }).catch(err => {
@@ -53,5 +58,21 @@ export class ScanFoodBarcodeComponent implements OnInit {
     if (role === 'confirm') {
       await this.trackingService.addFood(data.food, data.quantity);
     }
+  }
+
+  private async displayNotFound() {
+    const toast = await this.toastController.create({
+      position: 'bottom',
+      duration: 3000,
+      buttons: [
+        {
+          text: 'Dismiss',
+          role: 'cancel'
+        }
+      ],
+      message: 'Продуктът не е намерен'
+    });
+
+    await toast.present();
   }
 }
