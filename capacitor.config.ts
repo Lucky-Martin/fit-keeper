483f146dea69b1a56396dd7aa5217f9ed68438cb
Lucky-Martin <luckymartinlc@gmail.com>
2023-02-21 18:29:18 +0200

    Working build v1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,19 @@ const config: CapacitorConfig = {
   appId: 'com.fit.keeper',
   appName: 'fit-keeper',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    GoogleAuth: {
+      scopes: [
+        'profile',
+        'email'
+      ],
+      serverClientId: '796792034085-p3ut7338emlh34vmt72828ggle6fu5ki.apps.googleusercontent.com',
+      iosClientId: '796792034085-0emg47drs3ak44nho0q9drj1215b8lff.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true
+    }
+  },
+  cordova: {}
 };
 
 export default config;
